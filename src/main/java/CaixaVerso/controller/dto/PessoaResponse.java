b7ed6d666750c6ea325942cb8f6633fefc339e94
Rodrigo Gomes <rodrigo.gomes2294@gmail.com>
2026-09-25 10:29:38 -0300

    Meus primeiros testes na API Bancária

--- src/main/java/CaixaVerso/controller/dto/PessoaResponse.java
+++ src/main/java/CaixaVerso/controller/dto/PessoaResponse.java
@@ -6,7 +6,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public record PessoaResponse(
         @Schema(example = "1") Long id,
         @Schema(example = "Filipe Coordenador") String nome,
-        @Schema(example = "123456790") String cpf,
+        @Schema(example = "13221739799") String cpf,
         @Schema(example = "filipecoordenador@caixa.gov.br") String email
 ) {
     public static PessoaResponse de(Pessoa pessoa){
--- src/main/java/CaixaVerso/exception/CpfJaCadastradoException.java
+++ src/main/java/CaixaVerso/exception/CpfJaCadastradoException.java
@@ -3,5 +3,10 @@ package CaixaVerso.exception;
 public class CpfJaCadastradoException extends RuntimeException {
     public CpfJaCadastradoException() {
         super("CPF já cadastrado");
+
+
     }
+
 }
+
+
